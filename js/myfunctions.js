8781b28f25f3d7d4c95d579600bd37046bcf9051
Matan Tenenboim <matante@edu.hac.ac.il>
2021-11-07 23:22:34 +0200

    added card

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -3,7 +3,7 @@
 // /* holds information regarding a task, such as: priority, text, completed (should appear)*/
 class Todo {
     constructor() {
-        this.text = "";
+        this.title = "";
         this.description = ""
         this.priority = false;
         this.completed = false;
@@ -11,7 +11,7 @@ class Todo {
 
     addText() {
         let title = document.getElementById("todoTitle");
-        this.text = title.value;
+        this.title = title.value;
 
         let description = document.getElementById("todoDescription");
         this.description = description.value
@@ -27,15 +27,19 @@ class Todo {
 
     }
 
-    getTask() {
-        return this.text;
+    getTaskTitle() {
+        return this.title;
+    }
+
+    getTaskDesc() {
+        return this.description;
     }
 
     setPriority(priority) {
         this.priority = priority;
     }
 
-    getPriority() {
+    isPrioritized() {
         return this.priority;
     }
 
@@ -52,7 +56,36 @@ const tasks = [];
 
 function addNewTodo() {
     tasks.push(new Todo());
-    tasks[tasks.length - 1].addText();
+    const currTask = tasks[tasks.length - 1]
+    currTask.addText();
+
+    const list = document.getElementById("todosList")
+    const div1 = document.createElement("div")
+    const div2 = document.createElement("div")
+    const h5 = document.createElement("h5")
+    const para = document.createElement("p")
+
+    div1.classList.add("card")
+    if (currTask.isPrioritized()) {
+        div1.classList.add("bg-danger")
+        div1.classList.add("bg-opacity-25")
+        // div1.style.background.opacity = "0.25"
+    }
+
+
+    h5.classList.add("card-title")
+    h5.textContent = currTask.getTaskTitle()
+
+    para.classList.add("card-text")
+    para.textContent = currTask.getTaskDesc()
+
+    div2.append(h5)
+    div2.append(para)
+
+    div1.append(div2)
+    list.append(div1)
+
+
 }
 
 
@@ -62,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
         addNewTodo()
     });
     let prioBtn = document.getElementById("priorityBtn");
-    prioBtn.addEventListener('click', function (){
+    prioBtn.addEventListener('click', function () {
         //prioBtn.classList.toggle("btn-Success");
 
     })
@@ -70,5 +103,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+function displayTodos() {
+
+    document.getElementById("list").value = tasks[0].text
+}
 
 console.log("dddddddddddd")
+//displayTodos()
